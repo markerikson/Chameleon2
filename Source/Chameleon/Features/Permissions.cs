54b57be292a27fe1ccfec8094ffc267925aae661
Mark Erikson <mark@isquaredsoftware.com>
2010-06-19 21:57:50 +0000

    Forgot to add this with the last commit.

---
+++ Source/Chameleon/Features/Permissions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chameleon;
+
+namespace Chameleon.Features
+{
+	[Flags]
+	public enum ChameleonFeatures
+	{
+		NoFeature				= 0,
+		Feature1				= 1 << 0,
+		Feature2				= 1 << 1,
+		DragDropSnippets		= 1 << 2,
+	}
+
+	public class Permissions
+	{
+	}
+}
