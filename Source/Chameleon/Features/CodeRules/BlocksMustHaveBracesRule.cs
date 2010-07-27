86976ae1709a6665a488eea071f708aa37d320bc
Mark Erikson <mark@isquaredsoftware.com>
2010-07-27 22:39:49 +0000

    I apparently need to get into the habit of adding and committing files as soon as I create them.

---
+++ Source/Chameleon/Features/CodeRules/BlocksMustHaveBracesRule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Chameleon.Parsing;
+using DevInstinct.Patterns;
+using ScintillaNet;
+using Chameleon.GUI;
+using Chameleon.Util;
+using CodeLite;
+
+namespace Chameleon.Features.CodeRules
+{
+	class BlocksMustHaveBracesRule : CodeRuleBase
+	{
+		private BlocksMustHaveBracesRule()
+		{
+
+		}
+
+
+		public override bool ExamineSource(ChameleonEditor ed, Range searchRange)
+		{
+			bool ruleResult = true;
+
+			string[] blockKeywords = new string[] { "if", "else", "for", "while" };
+
+
+			return ruleResult;
+		}
+	}
+}
