1cb3070547b500a4746a2d3d32145c1cacddee90
Mark Erikson <mark@isquaredsoftware.com>
2010-08-20 01:34:40 +0000

    Adding the ExceptionReport library

---
+++ Libraries/ExceptionReporter/WinFormsProperties/AssemblyInfo.cs
@@ -0,0 +1,17 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: InternalsVisibleTo("ExceptionReporter.Tests")]
+[assembly: AssemblyTitle("ExceptionReporter.WinForms")]
+[assembly: AssemblyDescription("Displays exception details and system information")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("www.codeplex.com/ExceptionReporter")]
+[assembly: AssemblyProduct("Exception Reporter")]
+[assembly: AssemblyCopyright("Copyright © 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("C7B05A44-E28D-4a3b-AF52-34DFBD1C2E5F")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
---
+++ Libraries/ExceptionReporter/WpfProperties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: InternalsVisibleTo("ExceptionReporter.Tests")]
+[assembly: AssemblyTitle("ExceptionReporter.Wpf")]
+[assembly: AssemblyDescription("Displays exception details and system information")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("www.codeplex.com/ExceptionReporter")]
+[assembly: AssemblyProduct("Exception Reporter")]
+[assembly: AssemblyCopyright("Copyright © 2008")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("0C5ADEDB-7E7E-4f92-AD3B-A374B853931F")]
+[assembly: AssemblyVersion("2.1.0.0")]
+[assembly: AssemblyFileVersion("2.1.0.0")]
+
