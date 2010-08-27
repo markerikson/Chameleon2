03ab537ff4b97601362e9d52f3083f7cf17ba62c
Mark Erikson <mark@isquaredsoftware.com>
2010-08-27 00:32:43 +0000

    Redid the build script to use the "subwcrev" tool for inserting the current revision as part of Chameleon's version.

--- Build/buildChameleon.bat
+++ Build/buildChameleon.bat
@@ -1,1 +1,7 @@
-%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe useSVNVersion.proj
+set props=..\Source\Chameleon\Properties
+
+copy %props%\AssemblyInfo.cs %props%\AssemblyInfo.cs.bak /Y
+subwcrev . %props%\AssemblyInfo_template.cs %props%\AssemblyInfo.cs
+%WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ChameleonBuild.proj
+copy  %props%\AssemblyInfo.cs.bak %props%\AssemblyInfo.cs /Y
+del %props%\AssemblyInfo.cs.bak
--- Source/Chameleon/Properties/AssemblyInfo.cs
+++ Source/Chameleon/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Chameleon")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("The Adaptive Instructional IDE")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("ISquared Software")]
 [assembly: AssemblyProduct("Chameleon")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.5.0.0")]
-[assembly: AssemblyFileVersion("0.5.0.0")]
+[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
