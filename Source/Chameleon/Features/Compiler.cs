44d49ae1d046b8cd050da2ddecc8ff8775463ae6
Mark Erikson <mark@isquaredsoftware.com>
2010-09-30 16:17:20 +0000

    Yep, yet another new file that I forgot to add.

---
+++ Source/Chameleon/Features/Compiler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Chameleon.Features
+{
+	public class Compiler
+	{
+		private readonly string COMPILE_SUCCESS_TOKEN = "C_O_M_P_I_L_E_SUCCESS";
+		private readonly string COMPILE_FAILURE_TOKEN = "C_O_M_P_I_L_E_FAILED";
+	}
+}
