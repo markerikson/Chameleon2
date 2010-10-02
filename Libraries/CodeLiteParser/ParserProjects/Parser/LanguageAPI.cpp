ab2a6724b41018766a77422b4895cce00699b99d
Mark Erikson <mark@isquaredsoftware.com>
2010-10-02 19:24:53 +0000

    I completely forgot to implement the C DLL passthrough functions for LanguageWrapper.  Oops.

--- Libraries/CodeLiteParser/ParserProjects/Parser/LanguageAPI.cpp
+++ Libraries/CodeLiteParser/ParserProjects/Parser/LanguageAPI.cpp
@@ -11,27 +11,31 @@ static ExpressionResult tempResult;
 
 const wchar_t* CLP_L_OptimizeScope(const wxString& srcString)
 {
+	tempString = LanguageST::Get()->OptimizeScope(srcString);
 	return tempString.wc_str();
 }
 
 const wchar_t* CLP_L_GetScopeName(const wxString &in, std::vector<wxString> *additionlNS)
 {
+	tempString = LanguageST::Get()->GetScopeName(in, additionlNS);
 	return tempString.wc_str();
 }
 
 
 ExpressionResult* CLP_L_ParseExpression(const wxString &in)
 {
+	tempResult = LanguageST::Get()->ParseExpression(in);
 	return &tempResult;
 }
 
 bool CLP_L_ProcessExpression(const wxString& stmt, const wxString& text, const wxString &fn, int lineno, wxString &typeName, wxString &typeScope, wxString &oper, wxString &scopeTemplateInitList)
 {
-	return false;
+	bool result = LanguageST::Get()->ProcessExpression(stmt, text, fn, lineno, typeName, typeScope, oper, scopeTemplateInitList);
+	return result;
 }
 
 void CLP_L_GetLocalVariables(const wxString &in, std::vector<TagEntryPtr> &tags, const wxString &name, size_t flag)
 {
-
+	LanguageST::Get()->GetLocalVariables(in, tags, name, flag);
 }
 
