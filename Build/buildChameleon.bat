b7103790c5bfc21aef1fce69b3c665f911553482
Mark Erikson <mark@isquaredsoftware.com>
2010-10-02 21:41:10 +0000

    Use Chameleon source code folder as source for actual build SVN revision number (since we want to ignore tags, release/update commits, etc).

--- Build/buildChameleon.bat
+++ Build/buildChameleon.bat
@@ -1,7 +1,7 @@
 set props=..\Source\Chameleon\Properties
 
 copy %props%\AssemblyInfo.cs %props%\AssemblyInfo.cs.bak /Y
-subwcrev .. %props%\AssemblyInfo_template.cs %props%\AssemblyInfo.cs
+subwcrev ..\Source\Chameleon %props%\AssemblyInfo_template.cs %props%\AssemblyInfo.cs
 %WINDIR%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe ChameleonBuild.proj
 copy  %props%\AssemblyInfo.cs.bak %props%\AssemblyInfo.cs /Y
 del %props%\AssemblyInfo.cs.bak
